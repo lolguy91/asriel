dd89e1f77ab6a2ad8ec8a3197b46f7434c029bb4
lolguy91 <retek1544@gmail.com>
2024-09-23 17:34:30 +0200

    test and gitignore

--- kernel-mockup/src/main.c
+++ kernel-mockup/src/main.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+#include "object_manager.h"
+
 int main(int argc, char *argv[]) {
-    printf("Let's make a mockup of the Object Manager and VFS!\n");
+    if(!object_manager_init()){
+        printf("Failed to initialize object manager\n");
+        return 1;
+    }
+
+    printf("Object manager initialized\n");
+    device_t dev = {
+        .name = "test",
+        .type = 0,
+    };
+    obj_to_insert_t obj = {
+        .type = 0,
+        .data.device = dev
+    };
+    object_manager_insert(obj);
+
+    return 0;
 }
