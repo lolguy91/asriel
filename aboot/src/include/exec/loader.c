344860b3475f71520dcdb353c40498eefc3a3979
RaphtikAtGHG <raphael.wegmann81@gmail.com>
2024-09-26 16:57:26 +0200

    Added some loader stuff

--- aboot/src/include/exec/loader.c
+++ aboot/src/include/exec/loader.c
@@ -1,1 +1,9 @@
 #include "loader.h"
+
+void loader_load(const char* filename) {
+    uint64_t filesize;
+    void *filebuf;
+    EFI_STATUS status;
+
+    // TODO: make everything
+}
--- aboot/src/include/exec/loader.h
+++ aboot/src/include/exec/loader.h
@@ -1,4 +1,9 @@
 #ifndef __LOADER_H__
 #define __LOADER_H__
 
+#include <common.h>
+#include <stdint.h>
+
+void loader_load(const char* filename);
+
 #endif // __LOADER_H__
