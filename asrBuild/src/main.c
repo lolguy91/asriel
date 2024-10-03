72a9c56130e62240ba4b6b8aef6bf609c0c9dccf
lolguy91 <retek1544@gmail.com>
2024-10-03 16:45:27 +0200

    I just realized all that effort trying to figure out how to concat the path with "/build.lua" was not needed

--- asrBuild/src/main.c
+++ asrBuild/src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#include "logger.h"
+#include "util.h"
 
 #include "script.h"
 
@@ -10,10 +14,16 @@ int main(int argc, char** argv){
     if (argc >= 2){
         path = argv[1];
     }
-    char* fullpath = malloc(strlen(path) + strlen("/build.lua") + 1);
-    memset(fullpath,0,strlen(path) + strlen("/build.lua") + 1);
-    strcpy(fullpath,path);
-    strcat(fullpath,"/build.lua");
 
-    ab_runscript(fullpath,argc,argv);
+    if(chdir(path) != 0){
+        log_err("Failed to change directory to the project path",__CUR_LOC);
+        exit(EXIT_FAILURE);
+    }
+
+    //check if build.lua exists
+    if (access("build.lua", F_OK) == 0) {
+        ab_runscript("build.lua",argc,argv);
+    }
+
+    return 0;
 }
