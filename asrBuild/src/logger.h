7581c7e7e7985b186abb45e53d591eb663a7029f
lolguy91 <retek1544@gmail.com>
2024-10-04 14:07:28 +0200

    lol I forgot to add the docs to the headers

--- asrBuild/src/logger.h
+++ asrBuild/src/logger.h
@@ -1,7 +1,16 @@
 #ifndef __LOGGER_H__
 #define __LOGGER_H__
-
+/*
+ * @brief Logs info
+ * @param `message` The message to log
+ * @param `location` The location of the message
+*/
 void log_info(const char* message,const char* location);
+/*
+ * @brief Logs errors
+ * @param `message` The message to log
+ * @param `location` The location of the message
+*/
 void log_err(const char* message,const char* location);
 
 
